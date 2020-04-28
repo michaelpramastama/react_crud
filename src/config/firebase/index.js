9ddd5f39f5fcb19c79600160841c5005799a7054
michaelpramastama <michaelwidap@gmail.com>
2020-04-28 15:52:31 +0700

    add config firebase
    instal npm firebase
    "npm install --save firebase"

---
+++ src/config/firebase/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDoXSQhXxWyCYME5zyRG79r6RpEy1Eonqo",
+    authDomain: "crud-reactt.firebaseapp.com",
+    databaseURL: "https://crud-reactt.firebaseio.com",
+    projectId: "crud-reactt",
+    storageBucket: "crud-reactt.appspot.com",
+    messagingSenderId: "573546867254",
+    appId: "1:573546867254:web:3ab1c06c2b78d3c2a08697",
+    measurementId: "G-PWGCNGV5EG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
